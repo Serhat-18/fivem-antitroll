9bf0ebab3958423421258954338befe9d80f6bf5
Serhat-18 <79026362+Serhat-18@users.noreply.github.com>
2024-01-25 23:35:29 +0300

    Add files via upload

---
+++ Vdm.lua
@@ -0,0 +1,32 @@
+-- OBFUSCATOR: SRHT OBFUSCATOR V2 http://www.srhdev.xyz/Site/sifrele/lua/dist/index.html
+
+-- Discord Webhook URL
+local burayaucurlogi = "WEBHOOK_HERE"
+
+
+-- ARAC HASARINI 0 A CEKME  CLIENTE EKLEYIN EGER ISTIYORSANIZ --
+
+
+
+
+--[[
+Citizen.CreateThread(function()
+    while true do
+        N_0x4757f00bc6323cfe(-1553120962, 0.0) 
+        Wait(0)
+    end
+end)
+--]]
+
+
+
+
+-- CARPMA ALGILAMA --
+AddEventHandler('playerBeenHit', function(hitData, vehicle)
+    if IsEntityAVehicle(vehicle) then
+        local Trollsupheli = GetPlayerName(source)
+        local aracmodel = GetDisplayNameFromVehicleModel(GetEntityModel(vehicle))
+        local messageaq = "[SRHT_GUARD_V3] - [VDM] "..Trollsupheli .. "Şu araçla vurdu " .. aracmodel.."Vdm şüphesi var kontrol etmeniz önerilir"
+        PerformHttpRequest(burayaucurlogi, function(err, text, headers) end, 'POST', json.encode({content = messageaq}), { ['Content-Type'] = 'application/json' })
+    end
+end)
